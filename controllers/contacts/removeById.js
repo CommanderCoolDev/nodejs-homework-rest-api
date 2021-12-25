b9539736c8a3338e8b4fe07d8e42c641002940f1
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2021-12-25 16:09:31 +0200

    hw03 remove/add

--- controllers/contacts/removeById.js
+++ controllers/contacts/removeById.js
@@ -1,18 +1,18 @@
-const { NotFound } = require('http-errors');
+const { NotFound } = require("http-errors");
 
-const { contactsOperation } = require('../../model');
+const { Contact } = require("../../models");
 
 const removeById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contactsOperation.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   console.log(result);
   if (!result) {
     throw new NotFound(`Contact with id='${contactId}' not found`);
   }
   res.json({
-    status: 'success',
+    status: "success",
     code: 200,
-    message: 'contact deleted ',
+    message: "contact deleted ",
     data: {
       result,
     },
