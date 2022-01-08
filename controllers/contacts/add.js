81b3a66adf3d0862f0a7c54ee8656672a19ee739
Dima <78321515+CommanderCoolDev@users.noreply.github.com>
2022-01-08 11:46:03 +0200

    hw04 added task

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -1,9 +1,11 @@
-const { Contact } = require('../../models');
+const { Contact } = require("../../models");
 
 const add = async (req, res) => {
-  const result = await Contact.create(req.body);
+  const { _id } = req.user;
+  const result = await Contact.create({ ...req.body, owner: _id });
+
   res.status(201).json({
-    status: 'success',
+    status: "success",
     code: 201,
     data: {
       result,
